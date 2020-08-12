68f954e34a84c18bce97136249a4b18f11697a84
László Ceglédi <laszlo.cegledi@heitec.hu>
2020-08-12 09:11:46 +0200

    modules and services added

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -4,12 +4,16 @@ import { AppService } from './app.service';
 import { TodoModule } from './todo/todo.module';
 import { AutomapperModule } from 'nestjsx-automapper';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { UsersModule } from './users/users.module';
+import { AuthModule } from './auth/auth.module';
 
 @Module({
   imports: [
     TodoModule,
     AutomapperModule.withMapper(),
     TypeOrmModule.forRoot(),
+    UsersModule,
+    AuthModule,
   ],
   controllers: [AppController],
   providers: [AppService],
